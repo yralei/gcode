91b43f356a7661de9fb454c82c6f2408f1b25139
Iurii <yralei@gmail.com>
2021-05-17 15:41:51 +0300

    Operators task 1

---
+++ 4. Operators/task 1 Arithmetic operators.js
@@ -0,0 +1,24 @@
+'use strict';
+const income = 1000;
+
+const rent = 400;
+let otherExpenses = 500;
+
+let balance = income - rent - otherExpenses;
+
+console.log(balance);
+
+// ---------------
+
+let freelanceIncome = 200;
+
+// put your code here
+
+freelanceIncome = freelanceIncome * 2;
+console.log(freelanceIncome);
+
+balance = balance + freelanceIncome;
+console.log(balance);
+
+balance = balance / 2;
+console.log(balance);
