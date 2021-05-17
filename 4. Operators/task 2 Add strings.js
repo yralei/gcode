822ae0b4fb3b442153bf6975481361ed1c62dca7
Iurii <yralei@gmail.com>
2021-05-17 16:19:14 +0300

    Operators task 2

---
+++ 4. Operators/task 2 Add strings.js
@@ -0,0 +1,7 @@
+'use strict';
+const userName = 'Andrey';
+const currentDay = 'Monday';
+
+// put your code here
+let welcomeMessage = ('Hello, ' + userName + '! It is ' + currentDay);
+console.log(welcomeMessage);
