365d8f5e1b774c9e0079ee2ca6e26d7fda531114
Iurii <yralei@gmail.com>
2021-05-18 14:04:19 +0300

    Operators task 7

---
+++ 4. Operators/task 7 Use correct operators.js
@@ -0,0 +1,20 @@
+const income = 1000;
+
+const rent = 400;
+let otherExpenses = 500;
+
+let balance = income - rent - otherExpenses;
+
+console.log(balance);
+
+// ---------------
+
+let freelanceIncome = 200;
+freelanceIncome *= 2;
+console.log(freelanceIncome);
+
+balance += freelanceIncome;
+console.log(balance);
+
+balance /= 2;
+console.log(balance);
