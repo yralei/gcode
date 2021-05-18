0e84b42cc6fb0b02eeb77bd82da94b91f3185668
Iurii <yralei@gmail.com>
2021-05-18 16:49:08 +0300

    functions task2

---
+++ 5. Functions/task 1 say welcome.js
@@ -0,0 +1,5 @@
+// put your code here
+function sayWelcome() {
+  console.log('Hello, user! Have a good day!');
+}
+sayWelcome();
---
+++ 5. Functions/task 2 Say welcome with params.js
@@ -0,0 +1,9 @@
+// put your code here
+function sayWelcome() {
+  console.log('Hello, ' + userName + '! Have a good ' + currentDay + '!');
+  userName = 'Iurii';
+  currentDay = 'Monday';
+}
+sayWelcome();
+sayWelcome();
+sayWelcome();
