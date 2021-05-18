a595691623aa99f1da26b2d34bc967e14df114e0
Iurii <yralei@gmail.com>
2021-05-18 12:09:44 +0300

    Operators task 3

---
+++ 4. Operators/task 3 Comparison operators.js
@@ -0,0 +1,29 @@
+//my balance
+const balance = 15600;
+
+// describe some product
+const id = 1001;
+let productName = 'IPhone 12 Pro Max';
+let isAvailable = true;
+let price = 20000;
+const color = 'Silver';
+let storeAddress = null;
+
+/**
+ * Может ли пользователь купить конкретный товар? Выведи результат проверки в консоль
+ */
+
+// put your code here
+console.log(balance >= price);
+/**
+ * Есть ли товар в наличии? Выведи результат проверки в консоль
+ */
+
+// put your code here
+console.log(isAvailable);
+/**
+ * Является ли цвет выбранного товара Silver. Выведи результат проверки в консоль
+ */
+
+// put your code here
+console.log(color==='Silver');
