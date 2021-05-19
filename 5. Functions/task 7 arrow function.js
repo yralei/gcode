2f1f367921a7a1d9cdc67ab5dfbe2c71cadf974e
Iurii <yralei@gmail.com>
2021-05-19 09:20:12 +0300

    functions task7

---
+++ 5. Functions/task 7 arrow function.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} firstNumber
+ * @param {number} secondNumber
+ * @return {number}
+ */
+ const min = (firstNumber, secondNumber) => {
+  // put your code here
+if (firstNumber < secondNumber) {
+  return firstNumber;
+} else {
+  return secondNumber;
+}
+};
+console.log(min(100, 40));
+console.log(min(30, 50));
