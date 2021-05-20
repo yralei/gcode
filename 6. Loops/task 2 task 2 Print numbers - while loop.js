f227814adfa8577044ea9b3b0e8e974ca556bfa4
Iurii <yralei@gmail.com>
2021-05-20 18:23:43 +0300

    loops task3

---
+++ 6. Loops/task 2 task 2 Print numbers - while loop.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {void}
+ */
+ function printNumbers(num) {
+  //put your code here
+  let index = 0;
+  while (index <= num) {
+      console.log(index);
+      index += 1;
+  }
+}
+printNumbers(25);
