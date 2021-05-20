cac54d2765498e4be4c493c9fe7a9dc9ec725ee3
Iurii <yralei@gmail.com>
2021-05-20 15:13:14 +0300

    loops task2

---
+++ 6. Loops/task 1 Print numbers - for loop.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {void}
+ */
+ function printNumbers(num) {
+  //put your code here
+  for (let index = 0; index <= num; index +=1) {
+    console.log(index);
+  }
+}
+printNumbers(25);
