54bebc6ffe9a5f29095cd5cf26c0d35f39a2fecb
Iurii <yralei@gmail.com>
2021-06-14 16:32:31 +0300

    arrays task1

---
+++ 9. Arrays/1.arraysBasics.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const students = ['Andrey', 'Denis', 'Alex', 'Nastya', 'Ninja', 'Skywoker', 'Joda', 'Leo'];
+
+// put your code here
+const thirdEl = students[2];
+console.log(thirdEl);
+
+const sixthEl = students[5];
+console.log(sixthEl);
+
+console.log(students[0]);
+
+const arrLength = students.length;
+console.log(students[arrLength - 1]);
