813a8317ff944f86761d0b931cb38109d3ab4a80
Iurii <yralei@gmail.com>
2021-06-21 12:28:00 +0300

    11.objects task 2

---
+++ 11. Objects/2.iterateObject.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @param {object} obj
+ * @return {string[]}
+ */
+function getKeys(obj) {
+  return Object.keys(obj) 
+}
+
+
+/**
+ * @param {object} obj
+ * @return {array}
+ */
+function getValues(obj) {
+  return Object.values(obj)
+}
